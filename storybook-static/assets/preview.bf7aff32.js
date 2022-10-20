03d66b370d0eda763a5b150a5723f034cfa50d3c
Leonardo Campos <campos.l@aluno.ifsp.edu.br>
2022-10-19 22:48:19 -0300

    Initial commit

---
+++ storybook-static/assets/preview.bf7aff32.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.3e9ddb9f.js";import"./iframe.5af93969.js";import"./es.map.constructor.3437371a.js";import"./es.number.to-fixed.cd0f66f8.js";import"./index.fb54ac0f.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.bf7aff32.js.map
