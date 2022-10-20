3fec0ea59ec931c7dd47aa8fd6b7ef93a1c4f02d
Leonardo Campos <campos.l@aluno.ifsp.edu.br>
2022-10-20 00:56:10 -0300

    Use components to create a login page

---
+++ src/components/App.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { App, AppProps } from '../App'
+
+export default{
+    title: 'Components/App',
+    component: App,
+    args: {},
+    argTypes: {}
+} as Meta<AppProps>
+
+export const Default: StoryObj<AppProps> = {}
